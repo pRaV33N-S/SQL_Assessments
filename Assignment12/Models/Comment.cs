484f125c169538eecb8003f13601c5eab17ba68f
pRaV33N-S <praveen.srinivasan1@mphasis.com>
2023-08-28 15:48:58 +0530

    Assignment 12

---
+++ Assignment12/Models/Comment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Security.Policy;
+
+namespace Assignment12.Models
+{
+    [Table("Comment")]
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public string PostId { get; set; }
+    }
+}
---
+++ Assignment12/Models/Post.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Assignment12.Models
+{
+    [Table("Post")]
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public string Author { get; set; }
+    }
+}
