1601a5d2ea9c41f9107aeb3477c7193f5675aae9
pRaV33N-S <praveen.srinivasan1@mphasis.com>
2023-08-30 18:10:49 +0530

    Assignment 14

---
+++ Assignment14/Models/Player.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment14.Models
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int JerseyNumber { get; set; }
+        public string Position { get; set; }
+        public string Team{ get; set; }
+    }
+}
